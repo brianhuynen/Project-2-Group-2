771ad725bf3b9fb1bef5b3868d319ba57b87a2c2
HuskydoT <brianhuynen@planet.nl>
2016-09-21 13:39:48 +0200

    Finished the Piece class.

--- src/Piece.java
+++ src/Piece.java
@@ -1,5 +1,16 @@
-// TODO Change Piece class and make sure that it stores a rank, position and playerID
-
 public class Piece {
 	
+	private int rank, playerID;
+	private Position position;
+	
+	public Piece(int playerID, int rank){
+		this.playerID = playerID;
+		this.rank = rank;
+	}
+	
+	public void setPosition(Position pos){position = pos;}
+	public Position getPosition(){return position;}
+	public void setRank(int rank){this.rank = rank;}
+	public int getRank(){return rank;}
+	public int getPlayer(){return playerID;}
 }
