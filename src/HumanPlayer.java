6935f089ad742439e8dd73ba707c8eda8b8fbd81
HuskydoT <brianhuynen@planet.nl>
2016-09-22 12:53:45 +0200

    Some small changes in the Player classes.
    I will try to explain the changes as clear as possible, but forgive me
    for the fact my explaining is horrible :c
    
    Basically the Player classes store an array called piecesData, which is
    an Integer array storing the ranks of all pieces which you can place (0
    - 11) and how many of each piece you can place (the amount is hardcoded
    so far).
    
    Furthermore, it stores the locations of the Pieces in an array of Pieces
    called pieces. These arrays are stored in the Player class because the
    Board needs to know what piece to place and where to place that piece.
    The Pieces are created by the Piece class and basically created by the
    Player, the Board just places them on the stored location.

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,4 +1,11 @@
 
 public class HumanPlayer extends Player{
-	private int[] piecesData;
+	private int[] piecesData = {1,1,8,5,4,4,4,3,2,1,1,6};
+	private Piece[] pieces;
+	private String name;
+	
+	public HumanPlayer(String name){
+		this.name = name;
+	}
+	
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,5 @@
 // TODO add methods.
 public abstract class Player {
-
+	private int[] piecesData = {1,1,8,5,4,4,4,3,2,1,1,6};
+	private Piece[] pieces;
 }
