20e33822b4fdbb4b495c41a514ab219c27752edc
HuskydoT <brianhuynen@planet.nl>
2016-10-19 10:58:48 +0200

    Updated project layout...

--- src/AIPlayer.java
+++
@@ -1,4 +0,0 @@
-//Stays empty until period 2
-public class AIPlayer extends Player {
-
-}
--- src/Cell.java
+++
@@ -1,8 +0,0 @@
-//Cell state interface for the sake of better structure within the program
-public abstract class Cell {
-	public Piece content;
-	
-	public Piece getContent(){
-		return content;
-	}
-}
