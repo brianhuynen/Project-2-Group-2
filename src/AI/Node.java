2946ebfed1ef0e7087b6454d48625b65fa6f3fe8
Carolley <carolley@outlook.com>
2016-11-23 12:22:17 +0100

    Binary Tree

--- src/AI/Node.java
+++ src/AI/Node.java
@@ -3,11 +3,23 @@ package AI;
 public class Node {
 	
 	Node leftChild;
+	Node parent;
 	Node rightChild;
+	int content;
 	
-	public Node(Node leftChild, Node rightChild){
+	public Node(int content, Node parent, Node leftChild, Node rightChild){
 		this.leftChild = leftChild;
 		this.rightChild = rightChild;
+		this.parent = parent;
+		this.content = content;
+	}
+	
+	public void setContent(int i){
+		this.content = i;
+	}
+	
+	public int getContent(){
+		return content;
 	}
 	
 	public Node getRightChild(){
@@ -24,6 +36,14 @@ public class Node {
 	
 	public void setRightChild(Node n){
 		this.rightChild = n;
+	}
+	
+	public void setParent(Node p){
+		this.parent = p;
+	}
+	
+	public Node getParent(){
+		return parent;
 	}
 
 }
